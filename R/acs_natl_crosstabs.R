e63c0ca63d14377be9ce4acca75de7bc2196e5f6
Cecile Murray <29072007+murray-cecile@users.noreply.github.com>
2020-03-27 09:00:43 -0500

    pushing national crosstabs

--- R/acs_natl_crosstabs.R
+++ R/acs_natl_crosstabs.R
@@ -88,7 +88,7 @@ natl_renter_income <- renters %>%
 #===============================================================================#
 
 natl_cost_by_age <-  renters %>% 
-  mutate(cost_burden = renters$RENTGRS / HHINCOME,
+  mutate(cost_burden = (renters$RENTGRS * 12) / HHINCOME,
          burden_status = case_when(
            HHINCOME == 0 ~ "Zero income",
            cost_burden < 0.3 ~ "Not burdened",
@@ -107,17 +107,39 @@ natl_cost_by_age <-  renters %>%
   mutate(n_age = sum(n),
          age_share = n / n_age)
 
+natl_cost_by_age %>% 
+  ggplot(aes(x = burden_status,
+             fill = age_cat)) +
+  geom_col(aes(y = age_share)) +
+  scale_y_continuous(labels = scales::percent) +
+  coord_flip() +
+  labs(title = "Cost-burdened renters skew younger",
+       subtitle = "Share of individuals of each age by gross rent as a share of household income, 2018",
+       x = "",
+       y = "% of people in renter households",
+       fill = "Age group") +
+  terner_theme()
 
 #===============================================================================#
 # OCCUPATIONS
 #===============================================================================#
 
 occupations <- read.xlsx("data/acs/occupations.xlsx") %>%
-  select(-X5, acs_n) %>% 
+  select(-X5,
+         -acs_n,
+         -is_vulnerable) %>% 
   mutate(occ_code = str_pad(occ_code, width = 4, side = "left", pad = "0"))
 
-natl_renter_occp <- renters %>% 
-  mutate(cost_burden = RENT / HHINCOME,
+natl_occp <- data %>% 
+  count(OCC, wt = PERWT) %>% 
+  haven::zap_labels() %>% 
+  mutate(OCC = str_pad(OCC, width = 4, side = "left", pad = "0")) %>% 
+  left_join(occupations,
+            by = c("OCC" = "occ_code")) 
+
+# compute cost-burdened share
+natl_cost_burden_by_occp <- renters %>%
+  mutate(cost_burden = (RENT*12) / HHINCOME,
          burden_status = case_when(
            HHINCOME == 0 ~ "Zero income",
            cost_burden < 0.3 ~ "Not burdened",
@@ -131,42 +153,92 @@ natl_renter_occp <- renters %>%
                                            "30-50% cost burdened",
                                            "Not burdened"))) %>% 
   group_by(OCC) %>% 
-  count(pov_level, wt = PERWT) %>% 
-  haven::as_factor("both")
-  
-  
+  count(burden_status, wt = PERWT) %>%
+  group_by(OCC) %>% 
   mutate(n_occ = sum(n),
-         age_share = n / n_occ) %>% 
+         share_of_occ = n / n_occ) %>% 
   ungroup() %>% 
-  zap_labels() %>% 
+  haven::zap_labels() %>% 
   mutate(OCC = str_pad(OCC, width = 4, side = "left", pad = "0")) %>% 
   left_join(occupations,
             by = c("OCC" = "occ_code")) 
 
-natl_occp_cost_burden <- natl_renter_occp %>% 
-  mutate(
-    vulnerable_occ = case_when(
-      str_detect(occ_label, "EAT-") ~ "food service",
-      str_detect(occ_label, "CLN-") ~ "cleaning",
-      str_detect(occ_label, "PRS-") ~ "personal care",
-      str_detect(occ_label, "ENT-") ~ "entertainment",
-      str_detect(occ_label, "MED-") ~ "medical",
-      str_detect(occ_label, "HLS-") ~ "health assistance"
-    )) %>% 
-  group_by(vulnerable_occ) %>% 
-  summarize(n = sum(n),
-            n_occ = sum(n_occ),
-            acs_n = sum(acs_n))
-  
+
+
+# Try median rent
+natl_median_rent_by_occp <- renters %>% 
+  group_by(OCC) %>% 
+  summarize(median_rent = median(RENTGRS, wt = PERWT),
+            median_pearnings = median(INCEARN, wt = PERWT),
+            sample_size = n()) %>% 
+  ungroup() %>% 
+  haven::zap_labels() %>% 
+  mutate(OCC = str_pad(OCC, width = 4, side = "left", pad = "0")) %>% 
+  left_join(occupations,
+            by = c("OCC" = "occ_code")) 
+
+
+# try poverty level
+natl_pov_status_by_occp <- renters %>% 
+  group_by(OCC, pov_level) %>% 
+  summarize(n_people = sum(wt = PERWT),
+            sample_size = n()) %>% 
+  ungroup() %>% 
+  haven::zap_labels() %>% 
+  mutate(OCC = str_pad(OCC, width = 4, side = "left", pad = "0")) %>% 
+  left_join(occupations,
+            by = c("OCC" = "occ_code")) 
   
 
 #===============================================================================#
 # EXPORT
 #===============================================================================#
 
+
+README <- tibble(
+  "Sheet Name" = c("natl_age_table",
+                   "natl_cost_by_age",
+                   "natl_renter_income",
+                   "natl_renter_poverty",
+                   "natl_renter_pov_by_lvl"),
+  "Description" = c("# of individuals and # renters by age group",
+                    "# of renters by income category", 
+                    "# of renters by age and poverty status",
+                    "# of renters by age group and cost burden",
+                    "# of renters by age and % of poverty"),
+  "Notes" = c("", "", "" ,
+              "Cost burden = gross rent * 12 / household income",
+              "n_pov is the # of people in a given poverty category across ages")
+)
+
 write_to_excel("data/tables/national_ACS_crosstabs.xlsx",
-               c("natl_age_table",
-                 "natl_cost_by_age",
+               c("README",
+                 "natl_age_table",
                  "natl_renter_income",
                  "natl_renter_poverty",
+                 "natl_cost_by_age",
                  "natl_renter_pov_by_lvl"))
+
+
+README <- tibble(
+  "Sheet Name" = c("natl_occp",
+                   "natl_cost_burden_by_occp",
+                   "natl_median_rent_by_occ",
+                   "natl_pov_status_by_occp"),
+  "Description" = c("# of individuals by occupation",
+                    "# of renters by occupation and household cost burden",
+                    "Median household rent and personal earnings by occupation among renters",
+                    "# of renters by occupation by % of poverty"),
+  "Notes" = c("Universe for this is all people, not just renters",
+              "Cost burden = gross rent * 12 / household income",
+              "",
+              "Note this is household rent but personal earned income")
+)
+
+write_to_excel("data/tables/natl_occupations.xlsx",
+               c("README",
+                 "natl_occp",
+                 "natl_cost_burden_by_occp",
+                 "natl_median_rent_by_occ",
+                 "natl_pov_status_by_occp"))
+
--- R/get_pums.R
+++ R/get_pums.R
@@ -9,6 +9,24 @@ libs <- c("here",
           "purrr",
           "janitor",
           "httr",
-          "tidycensus")
+          "tidycensus",
+          "dtplyr")
 lapply(libs, library, character.only = TRUE)
 
+# read in files and drop replicate weights
+housing_file <- read_csv("data/acs/raw_pums/psam_h02.csv") %>%
+  select(-matches("GTP[[:digit:]]+"))
+person_file <- read_csv("data/acs/raw_pums/psam_p02.csv") %>% 
+  select(-matches("GTP[[:digit:]]+"))
+
+PERSON_VARS <- c("SERIALNO", "PUMA", "ST", "ADJINC", "PWGTP", "AGEP", "CIT",
+                 "SEX", "OCCP", "PERNP", "POVPIP")
+
+HOUSEHOLD_VARS <- c("SERIALNO", "ADJHSG", "ADJINC", "WGTP", "TYPE", "TEN")
+
+# per instructions in ACS PUMS documentation (translated from SAS)
+full_file <- left_join(person_file,
+                       housing_file,
+                       by = "SERIALNO") 
+
+rm(person_file, housing_file)
